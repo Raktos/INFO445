57b62f1e46b3c8756815b7a2750f82185d0c7667
Jason Ho <Raktos35@gmail.com>
2016-01-20 14:48:52 -0800

    Update insertCity.sql

--- scripts/storedProcedures/insertCity.sql
+++ scripts/storedProcedures/insertCity.sql
@@ -7,11 +7,53 @@ CREATE PROCEDURE uspInsertCity
 	@Country varchar(255)
 AS
 	BEGIN TRAN t1
-		INSERT INTO COUNTRY (CountryName)
+		DECLARE @CountryFind int;
+		DECLARE @RegionFind int;
+		DECLARE @CityFind int;
+		
+		SELECT @CountryFind = CountryID 
+			FROM COUNTRY 
+			WHERE CountryName LIKE @Country;
+			
+		IF @CountryFind IS NULL
+		BEGIN
+			INSERT INTO COUNTRY (CountryName)
 			VALUES (@Country);
-		INSERT INTO REGION (RegionName, CountryID)
-			VALUES (@Region, SCOPE_IDENTITY());
-		INSERT INTO CITY (CityName, RegionID)
-			VALUES (@City, SCOPE_IDENTITY());
+			@CountryFind = SCOPE_IDENTITY();
+		END
+		
+		SELECT @CountryFind = CountryID 
+			FROM COUNTRY 
+			WHERE CountryName = @Country;
+			
+		IF @CountryFind IS NULL
+		BEGIN
+			INSERT INTO COUNTRY (CountryName)
+			VALUES (@Country);
+			@CountryFind = SCOPE_IDENTITY();
+		END
+		
+		SELECT @RegionFind = RegionID 
+			FROM REGION 
+			WHERE RegionName = @Region
+			AND CountryID = @CountryFind;
+			
+		IF @RegionFind IS NULL
+		BEGIN
+			INSERT INTO REGION (RegionName)
+			VALUES (@Region, @CountryFind);
+			@RegionFind = SCOPE_IDENTITY();
+		END
+		
+		SELECT @CityFind = CityID 
+			FROM CITY 
+			WHERE CityName = @City
+			AND RegionID = @RegionFind;
+			
+		IF @CityFind IS NULL
+		BEGIN
+			INSERT INTO CITY (CityName)
+			VALUES (@City, @RegionFind);
+		END
 	COMMIT TRAN t1
 GO
