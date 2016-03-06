b2852e573d8080de43e17ea31a704cdd565e4b75
danielhuie <huied93@uw.edu>
2016-03-06 14:04:43 -0800

    modified SponsorContact and Airline inserts

--- scripts/storedProcedures/uspInsertAirline.sql
+++ scripts/storedProcedures/uspInsertAirline.sql
@@ -1,14 +1,22 @@
 CREATE PROCEDURE uspInsertAirline
-	@AirlineName varchar(255)
+	@AirlineName varchar(255),
+	@AirlineID int output
 AS
 BEGIN
 	BEGIN TRAN T1		
+		DECLARE @AirlineFind int;
 
-		INSERT INTO AIRLINE(AirlineName)
-		VALUES(@AirlineName)
-	
-	IF @@error <> 0
-		ROLLBACK TRAN T1
-	ELSE
-		COMMIT TRAN T1
+		SET @AirlineFind = (SELECT AirlineID FROM AIRLINE WHERE AirlineName = @AirlineName)
+
+		IF @AirlineFind IS NULL
+		BEGIN
+			INSERT INTO AIRLINE(AirlineName)
+			VALUES (@AirlineName);
+			SET @AirlineID = SCOPE_IDENTITY()
+		END
+
+		IF @@error <> 0
+			ROLLBACK TRAN T1
+		ELSE
+			COMMIT TRAN T1
 END
