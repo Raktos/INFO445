2a9341efb68b3f923f5be93ee7c7eeac18ba3514
Zhixing Wang <zhixingwang71@outlook.com>
2016-03-06 10:19:14 -0800

    add insertActivitityCategory and modified insertHotelType

---
+++ scripts/storedProcedures/insertActivity.sql
@@ -0,0 +1,23 @@
+USE AtlasTravel_FINAL;
+GO
+
+CREATE PROCEDURE uspInsertActivityCategory
+	@ActivityCategoryName varchar(255),
+	@ActivityCategoryDesc varchar(255),
+AS
+	BEGIN TRAN t1
+	
+		DECLARE @ActCateFind int;
+
+		SET @ActCateFind = (SELECT ActivityCategoryID FROM ACTIVITY_CATEGORY 
+							WHERE ActivityCategoryName = @ActivityCategoryName
+							AND ActivityCategoryDesc = @ActivityCategoryDesc);
+
+		IF NOT EXIST @ActCateFind
+		BEGIN
+			INSERT INTO ACTIVITY_CATEGORY(ActivityCategoryName, ActivityCategoryDesc)
+			VALUES(@ActivityCategoryName, @ActivityCategoryDesc);
+		END
+
+	COMMIT TRAN t1
+GO
--- scripts/storedProcedures/insertHotelType.sql
+++ scripts/storedProcedures/insertHotelType.sql
@@ -6,7 +6,18 @@ CREATE PROCEDURE uspInsertHotelType
 	@HotelTypeDesc varchar(255),
 AS
 	BEGIN TRAN t1
-		INSERT INTO HOTEL_TYPE(HotelTypeName, HotelTypeDesc)
-		VALUES(@HotelTypeName, @HotelTypeDesc);
+
+		DECLARE @HotelTypeFind int;
+
+		SET @HotelTypeFind = (SELECT HotelTypeID FROM HOTEL_TYPE
+							WHERE HotelTypeName = @HotelTypeName
+							AND HotelTypeDesc = @HotelTypeDesc);
+
+		IF NOT EXIST @HotelTypeFind
+		BEGIN
+			INSERT INTO HOTEL_TYPE(HotelTypeName, HotelTypeDesc)
+			VALUES(@HotelTypeName, @HotelTypeDesc);
+		END
+		
 	COMMIT TRAN t1
 GO
