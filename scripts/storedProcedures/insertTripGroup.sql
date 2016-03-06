a20d82b0b9316e9a1803fb6e11b4098070b21efa
danielhuie <huied93@uw.edu>
2016-03-06 14:47:22 -0800

    updated InsertTripGroup

--- scripts/storedProcedures/insertTripGroup.sql
+++ scripts/storedProcedures/insertTripGroup.sql
@@ -1,18 +1,42 @@
 CREATE PROCEDURE uspInsertTripGroup
+	@TripName varchar(255),
+	@MainLeaderFName varchar(255),
+	@MainLeaderLName varchar(255),
+	@CoLeaderFName varchar(255),
+	@CoLeaderLName varchar(255),
 	@GroupName varchar(255),
 	@GroupDesc varchar(255)
 AS
 BEGIN
 	SET NOCOUNT ON;
 	BEGIN TRAN t1
+		DECLARE @TripFind INT;
+		DECLARE @MainLeaderFind INT;
+		DECLARE @CoLeaderFind INT;
+
 		DECLARE @GroupID INT;
 		DECLARE @TripID INT;
 		DECLARE @GroupMainLeaderID INT;
 		DECLARE @GroupCoLeaderID INT;
 
 		BEGIN
-			SELECT @GroupID = ISNULL(MAX(GroupID), 0) + 1
-			FROM dbo.TRIP_GROUP
+			SET @TripFind = (SELECT TripID FROM TRIP WHERE TripName = @TripName)
+			IF @TripFind IS NULL
+			BEGIN
+				RETURN -1
+			END
+
+			SET @MainLeaderFind = (SELECT LeaderID FROM LEADER WHERE LeaderFName = @MainLeaderFName AND LeaderLName = @MainLeaderLName)
+			IF @MainLeaderFind IS NULL
+			BEGIN
+				RETURN -1
+			END
+
+			SET @CoLeaderFind = (SELECT LeaderID FROM LEADER WHERE LeaderFName = @CoLeaderFName AND LeaderLName = @CoLeaderLName)
+			IF @CoLeaderFind IS NULL
+			BEGIN
+				RETURN -1
+			END
 
 			SET @TripID = (SELECT TOP 1 TripID FROM TRIP ORDER BY NEWID());
 
@@ -20,8 +44,8 @@ BEGIN
 
 			SET @GroupCoLeaderID = (SELECT TOP 1 LeaderID FROM LEADER WHERE LeaderID <> @GroupMainLeaderID ORDER BY NEWID());
 
-			INSERT INTO TRIPGROUP(GroupID, TripID, GroupMainLeaderID, GroupCoLeaderID, GroupName, GroupDesc)
-			VALUES(@GroupID, @TripID, @GroupMainLeaderID, @GroupCoLeaderID, @GroupName, @GroupDesc)
+			INSERT INTO TRIPGROUP(TripID, GroupMainLeaderID, GroupCoLeaderID, GroupName, GroupDesc)
+			VALUES(@TripID, @GroupMainLeaderID, @GroupCoLeaderID, @GroupName, @GroupDesc)
 
 		END
 
