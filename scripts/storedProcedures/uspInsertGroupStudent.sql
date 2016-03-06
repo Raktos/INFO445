a0443c92729103d948337bfe5e7f45bc4fc3b749
danielhuie <huied93@uw.edu>
2016-03-06 14:52:22 -0800

    updated InsertGroupStudent

--- scripts/storedProcedures/uspInsertGroupStudent.sql
+++ scripts/storedProcedures/uspInsertGroupStudent.sql
@@ -1,7 +1,25 @@
 CREATE PROCEDURE uspInsertGroupStudent
+	@StudentFName VARCHAR(255),
+	@StudentLName VARCHAR(255),
+	@GroupName VARCHAR(255)
 AS
 BEGIN
 	BEGIN TRAN T1
+		DECLARE @StudentFind int;
+		DECLARE @GroupFind int;
+
+		SET @StudentFind = (SELECT StudentID FROM STUDENT WHERE StudentFName = @StudentFName AND StudentLName = @StudentLName)
+		IF @StudentFind IS NULL
+		BEGIN
+			RETURN -1
+		END
+
+		SET @GroupFind = (SELECT GroupID FROM TRIPGROUP WHERE GroupName = @GroupName)
+		IF @GroupFind IS NULL
+		BEGIN
+			RETURN -1
+		END
+
 		DECLARE @StudentID int;
 		DECLARE @GroupID int;
 
