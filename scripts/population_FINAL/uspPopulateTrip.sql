000f01db6f54e9234a28b65a2a81a779a8f57ac1
wangz58 <zhixingwang71@outlook.com>
2016-03-07 09:37:32 -0800

    Create uspPopulateTrip.sql

---
+++ scripts/population_FINAL/uspPopulateTrip.sql
@@ -0,0 +1,27 @@
+CREATE PROCEDURE uspPopulateTrip
+	@counter INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRAN T1
+
+		DECLARE @TripID INT;
+		DECLARE @SponsorContactID INT;
+		DECLARE @TripName VARCHAR(50);
+		DECLARE @TripDesc VARCHAR(100);
+
+		WHILE @counter > 0
+		BEGIN
+			
+			SET @TripName = 'Trip ' + (SELECT ISNULL(MAX(TripID), 0) + 2 FROM dbo.TRIP);
+			SET @SponsorContactID= (SELECT TOP 1 SponsorContactID FROM SPONSOR_CONTACT ORDER BY newid());
+			SET @TripDesc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut velit lorem, placerat varius arcu quis, ullamcorper sagittis quam. Nulla placerat purus ac suscipit fermentum. Curabitur pulvinar in quam tincidunt rutrum.';
+			
+			INSERT INTO dbo.TRIP (SponsorContactID, TripName, TripDesc)
+			VALUES (@SponsorContactID, @TripName, @TripDesc)
+			SET @counter = @counter - 1;
+			
+		END
+
+	COMMIT TRAN t1
+END
