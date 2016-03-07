8e4542a23ea7783e93c53bb4c654cacea357f5b1
unknown <iGuest@is-m440m27-16.ischool.uw.edu>
2016-03-07 09:55:14 -0800

    modified exec

--- scripts/execution/kevin_execs.sql
+++ scripts/execution/kevin_execs.sql
@@ -6,17 +6,19 @@ SET @newCity = 'Glacier'
 SELECT * FROM CITY WHERE CityName = @newCity
 
 EXEC @returnVal = dbo.uspInsertTripActivity 
-    @TripName = 'Trip 4',
+    @TripName = 'Trip 85',
 	@ActivityTypeName = 'Hike',
 	@ActivityCategoryName = 'Physical',
 	@CityName  = @newCity,
 	@RegionName = 'Montana',
 	@CountryName = 'United States',
-	@ActivityName = 'Glacier National Park Hike',
-	@ActivityDesc = 'Going into the national park for a day, hiking for 6 hours',
+	@ActivityName = 'National Park Hike',
+	@ActivityDesc = 'Hiking through Glacier National Park',
 	@ActivityStreetAddress = ' 545 Going-To-The-Sun Rd',
 	@ActivityStartDate = '2016-03-07',
 	@ActivityEndDate = '2008-03-07',
 	@ActivityCost = 15
 
 SELECT @returnVal
+
+SELECT TOP 1 * FROM TRIP_ACTIVITY ORDER BY TripActivityID DESC
